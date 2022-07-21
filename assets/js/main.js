499e64ff7ca926e13228a3a287874cbbc9a8a8e9
Samuel Alexandre Barbosa <samuelbhalexandre@hotmail.com>
2022-07-21 16:11:51 -0300

    adicionando outros arquivos

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+    
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc})`;
+
+    setResultado(msg, true);
+});
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do Peso', 'Peso Normal', 'Sobrepeso', 
+    'Obseidade Grau 1', 'Obseidade Grau 2', 'Obseidade Grau 3'];
+
+    if (imc > 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function getImc (peso, altura) {
+    const imc = peso / (altura ** altura);
+    return imc.toFixed(2);
+}
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado (msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    
+    const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
